fe3b60c88bd7ac678b63cbadc6f1fb64b8b388be
kwonjs1996 <kwonjs1996@gmail.com>
2022-04-17 23:01:13 +0900

    Create React App => module

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from "./Button";
+import styles from "./App.module.css";
+//css파일을 모듈화 할 수 있다. 모듈화 한 css를 가져왔을 때 임의의 ClassName이 들어간다
+//이건 엄청난 일이다 근데 왜인지는 잘 모르겠다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styles.title}>Welcome back</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }

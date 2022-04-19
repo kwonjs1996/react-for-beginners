9defde21d6a84ef1b232b203584229b56977d839
kwonjs1996 <kwonjs1996@gmail.com>
2022-04-19 11:24:13 +0900

    To Do List (...)배열안에 추가하기

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,22 +1,30 @@
-import { useState, useEffect } from "react";
-//css파일을 모듈화 할 수 있다. 모듈화 한 css를 가져왔을 때 임의의 ClassName이 들어간다
-//이건 엄청난 일이다 근데 왜인지는 잘 모르겠다. 아마도 파일을 분리해서 관리하기에 용이하기 때문?
-
-function Hello() {
-  useEffect(() => {
-    console.log("Hi");
-    return () => console.log("Bye");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To do</button>
+      </form>
     </div>
   );
 }

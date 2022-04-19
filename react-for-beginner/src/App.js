5f27f8c5d07f6649291f3ff224c5c4f7d18527fe
kwonjs1996 <kwonjs1996@gmail.com>
2022-04-19 10:54:22 +0900

    Clean up Fn

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,19 +1,22 @@
-//import Button from "./Button";
-//import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 //css파일을 모듈화 할 수 있다. 모듈화 한 css를 가져왔을 때 임의의 ClassName이 들어간다
 //이건 엄청난 일이다 근데 왜인지는 잘 모르겠다. 아마도 파일을 분리해서 관리하기에 용이하기 때문?
-function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
+
+function Hello() {
   useEffect(() => {
-    console.log("Call the API...");
+    console.log("Hi");
+    return () => console.log("Bye");
   }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

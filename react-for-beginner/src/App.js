b790daaa7f2c0d83a7e3696b5e31be58cc799f37
kwonjs1996 <kwonjs1996@gmail.com>
2022-04-21 19:40:43 +0900

    Movie App 컴포넌트 나누기

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,34 +1,7 @@
-import { useEffect, useState } from "react";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading....</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>{movie.title}</div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
